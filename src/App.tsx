fc1c9f16d062ccb2975e351a98b61785bbba592c
Lazlo Meli <lazlomeli@gmail.com>
2025-12-22 20:47:53 +0100

    chore: vcl si

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Index from "./pages/Index";
 import Facilities from "./pages/Facilities";
 import Pricing from "./pages/Pricing";
@@ -33,6 +34,7 @@ const App = () => (
         </BrowserRouter>
       </TooltipProvider>
       <Analytics />
+      <SpeedInsights />
     </QueryClientProvider>
   </HelmetProvider>
 );
